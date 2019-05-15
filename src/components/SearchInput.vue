800f3461ab002107937bd24456a84a9c02576819
Muhammad Azhaziq <azhaziqgoh23@outlook.com>
2019-05-15 10:53:58 +0800

    add title header

---
+++ src/components/SearchInput.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="search-input">
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'SearchInput',
+  props: {}
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped lang="scss">
+</style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,41 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+  <b-container fluid>
+    <b-row>
+      <b-col>
+        <div class="title-header">
+          <h4>Github Search</h4>
+        </div>
+      </b-col>
+    </b-row>
+  </b-container>
 </template>
 
 <script>
 // @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
+import SearchInput from '@/components/SearchInput.vue'
 
 export default {
   name: 'home',
   components: {
-    HelloWorld
+    SearchInput
   }
 }
 </script>
+
+<style lang="scss" scoped>
+  h4 {
+    color: #6f737b;
+    position: relative;
+  }
+
+  h4::after {
+    content: "";
+    display: block;
+    position: absolute;
+    height: 1px;
+    background: black;
+    width: calc(100% - 160px);
+    top: 56%;
+    right: 0;
+  }
+</style>
